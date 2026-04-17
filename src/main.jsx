c9ef1e329f83b2b3044311f200179f6b084a8039
AURA Dev <ayesha@aura.fashion>
2026-04-17 14:54:20 -0400

    Fix white screen on GitHub Pages by setting base path and router basename

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { ShopProvider } from './context/ShopContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/AuraDemo">
       <ShopProvider>
         <App />
       </ShopProvider>
